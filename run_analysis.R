cbe102d2396ab1bdadf777d7c878559d952c57d5
Hayes Williams <williamshayes@yahoo.com>
2015-01-21 13:49:32 -0500

    Create template and commented file.

---
+++ run_analysis.R
@@ -0,0 +1,30 @@
+## Getting and Cleaning Data - Course Project
+##
+## This R script will take data supplied by the UCI Machine Learning Repository
+## describing people engaged in various activities with a smartphone wearable
+## monitoring system.
+##
+## This script will take the following steps.
+## 1. Merge the training and the test sets to create one data set.
+## 2. Extract only the measurements on the mean and standard deviation for each 
+##    measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set.
+## 4. Appropriately label the data set with descriptive variable names. 
+## 5. From the data set in step 4, create a second, independent tidy data set 
+##    with the average of each variable for each activity and each subject.
+
+
+## 0. Download and stage the data supplied.
+
+## 1. Merge the training and test sets into one data set.
+
+## 2. Extract measurements on the mean and standard deviation
+
+## 3. Define activity names
+
+## 4. Label the data set
+
+## 5. Create the tidy data set
+
+
+
