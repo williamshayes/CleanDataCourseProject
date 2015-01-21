a4e88fefdbc7fd25e95595be95828d588389a68b
Hayes Williams <williamshayes@yahoo.com>
2015-01-21 15:25:19 -0500

    Added loading of data section. Started others.

--- run_analysis.R
+++ run_analysis.R
@@ -5,6 +5,8 @@
 ## monitoring system.
 ##
 ## This script will take the following steps.
+## 0. Retrieve the data from the remote source and load the data pieces into
+##    specific R data objects for later use by the script.
 ## 1. Merge the training and the test sets to create one data set.
 ## 2. Extract only the measurements on the mean and standard deviation for each 
 ##    measurement. 
@@ -14,9 +16,42 @@
 ##    with the average of each variable for each activity and each subject.
 
 
-## 0. Download and stage the data supplied.
+## 0(a). Download and stage the data supplied.
+fileURL<-paste("https://d396qusza40orc.cloudfront.net/",
+               "getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+               sep="")
+download.file(fileURL, "UCI_HAR_Dataset.zip")
+unzip("UCI_HAR_Dataset.zip")
+files <- list.files("UCI HAR Dataset", full.names=TRUE, recursive=TRUE)
+UCI_HAR_Dataset.download.date <- Sys.time()
+UCI_HAR_Dataset.download.date
+
+## 0(b). Load data from needed files into data frames for later use.
+## Get the full file names for all needed tables
+activityFile     <- grep("activity",      files, value=TRUE)
+xTestFile        <- grep("/X_test",       files, value=TRUE)
+yTestFile        <- grep("/y_test",       files, value=TRUE)
+xTrainFile       <- grep("/X_train",      files, value=TRUE)
+yTrainFile       <- grep("/y_train",      files, value=TRUE)
+featuresFile     <- grep("features\\.",   files, value=TRUE)
+subjectTestFile  <- grep("subject_test",  files, value=TRUE)
+subjectTrainFile <- grep("subject_train", files, value=TRUE)
+
+# Read the data into data objects for use later
+activityLabels <- read.table(activityFile)[,2]
+xTest          <- read.table(xTestFile)
+yTest          <- read.table(yTestFile)
+xTrain         <- read.table(xTrainFile)
+yTrain         <- read.table(yTrainFile)
+features       <- read.table(featuresFile)[,2]
+subjectTest    <- read.table(subjectTestFile)
+subjectTrain   <- read.table(subjectTrainFile)
 
 ## 1. Merge the training and test sets into one data set.
+names(xTest) <- features
+names(yTest) <- features
+names(xTrain) <- features
+names(yTrain) <- features
 
 ## 2. Extract measurements on the mean and standard deviation
 
